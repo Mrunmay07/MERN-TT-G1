ec44423e31f532166e82e46dc011590da6c09338
mrunmay-24-ukey <mrunmayukey587@gmail.com>
2026-04-06 11:43:29 +0530

    feat(react):Add all react code

--- React/project-2/src/components/CardsContainer.jsx
+++ React/project-2/src/components/CardsContainer.jsx
@@ -1,15 +1,32 @@
 import Card from "./Card";
 import seriesData from "../api/seriesData.json";
 import '../styles/CardsContainer.css'
+import { useEffect, useState } from "react";
 
 function CardsContainer() {
+
+  const [productsData , setProductsData] = useState([])
+
+
+  async function getProducts(){
+    const res = await fetch('https://dummyjson.com/products')
+    const data = await res.json()
+    console.log(data.products)
+    setProductsData(data.products)
+  }
+
+
+  useEffect(() => {
+      getProducts()
+  } , [])
+
   return (
     <div className="cards-container">
-      {seriesData.map((currSeries) => (
+      {productsData.map((currSeries) => (
         <Card key={currSeries.id}
           title={currSeries.title}
           description={currSeries.description}
-          image={currSeries.image}
+          image={currSeries.images[0]}
         />
       ))}
     </div>
---
+++ React/useRef/src/App.jsx
@@ -0,0 +1,12 @@
+import SignUp from "./components/SignUp";
+
+function App() {
+  return (
+    <>
+      <h1>SignUp page uncontrolled</h1>
+      <SignUp />
+    </>
+  );
+}
+
+export default App;
