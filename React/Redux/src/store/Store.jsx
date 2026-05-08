dbba72f6016c51cc64bddd24827800f5b1312a55
mrunmay-24-ukey <mrunmayukey587@gmail.com>
2026-05-08 10:36:51 +0530

    feat/Add redux todo app

--- React/Redux/src/store/Store.jsx
+++ React/Redux/src/store/Store.jsx
@@ -1,50 +1,46 @@
-import { composeWithDevTools } from '@redux-devtools/extension'
-import {createStore} from 'redux'
-
+import { composeWithDevTools } from "@redux-devtools/extension";
+import { createStore } from "redux";
 
 // actions
-const ADD_TASK = 'task/add'
-const DELETE_TASK = 'task/delete'
+const ADD_TASK = "task/add";
+const DELETE_TASK = "task/delete";
 
+const initialState = {
+  task: [],
+};
 
-    const initialState = {
-        task:[]
-    }
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case ADD_TASK:
+      return { ...state, task: [...state.task, action.payload] };
 
-function reducer(state = initialState , action){
-    switch (action.type) {
-        case ADD_TASK:
-            return {...state , task:[...state.task ,action.payload]}
-            
-        case DELETE_TASK:
-            const updatedArray = state.task.filter((curr , index) => {
-                return index !== action.payload
-            })
+    case DELETE_TASK:
+      const updatedArray = state.task.filter((curr, index) => {
+        return index !== action.payload;
+      });
 
-            return {...state , task:updatedArray}
-        
-    }
+      return { ...state, task: updatedArray };
+  }
 }
 
 // Creating a Global Store
-export const store = createStore(reducer , composeWithDevTools())
-console.log(store)
+export const store = createStore(reducer, composeWithDevTools());
+console.log(store);
 
 store.subscribe(() => {
-    console.log(store.getState())
-})
+  console.log(store.getState());
+});
 
 // action creators
-export function addTask(data){
-    return {type:ADD_TASK , payload:data}
+export function addTask(data) {
+  return { type: ADD_TASK, payload: data };
 }
 
-export function deleteTask(id){
-    return {type:DELETE_TASK , payload:id }
+export function deleteTask(id) {
+  return { type: DELETE_TASK, payload: id };
 }
 
-store.dispatch(addTask('Coding'))
-store.dispatch(addTask('Gaming'))
-store.dispatch(addTask('Study'))
-store.dispatch(deleteTask(1))
-
+store.dispatch(addTask("Coding"));
+store.dispatch(addTask("Gaming"));
+store.dispatch(addTask("Study"));
+store.dispatch(deleteTask(1));
