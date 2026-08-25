900e4823ae3039f3471ab8ac68ec3da781b73f2a
mrunmay-24-ukey <mrunmayukey587@gmail.com>
2026-08-25 10:48:11 +0530

    learn : Add all CRUD operations in blog app and user registration functionality

--- backend/03_blog_application/app.js
+++ backend/03_blog_application/app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import blogsData from "./blogsDB.json" with { type: "json" };
+import usersData from "./usersDB.json" with { type: "json" };
 import crypto from "node:crypto";
-import { writeFile } from "node:fs/promises"
-import path from "path"
-import multer from "multer"
+import { writeFile } from "node:fs/promises";
+import path from "path";
+import multer from "multer";
 
 const app = express(); // Object
 
@@ -11,17 +12,18 @@ app.use(express.json());
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads')
+    cb(null, "./uploads");
   },
   filename: function (req, file, cb) {
-    const id = crypto.randomUUID()
-    const extension = path.extname(file.originalname)
-    cb(null, `${id}${extension}`)
-  }
-})
+    const id = crypto.randomUUID();
+    const extension = path.extname(file.originalname);
+    cb(null, `${id}${extension}`);
+  },
+});
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
+// READ
 app.get("/", (req, res) => {
   res.json({ message: "Hello world" });
 });
@@ -37,9 +39,10 @@ app.get("/blogs/:id", (req, res) => {
   res.json(blog);
 });
 
-app.post("/blogs", upload.single("image") ,async (req, res) => {
+// CREATE 
+app.post("/blogs", upload.single("image"), async (req, res) => {
   const { title, content, author } = req.body;
-  console.log(req.body)
+  console.log(req.body);
   if (!title || !content || !author) {
     return res.status(400).json({ message: "All fields are requried" });
   }
@@ -49,7 +52,7 @@ app.post("/blogs", upload.single("image") ,async (req, res) => {
     title,
     content,
     author,
-    like:0,
+    like: 0,
     comment: [],
     image: req.file ? `/uploads/` : null,
     createdAt: new Date().toISOString(),
@@ -59,38 +62,133 @@ app.post("/blogs", upload.single("image") ,async (req, res) => {
   blogsData.push(newBlog);
 
   try {
-    await writeFile("./blogsDB.json", JSON.stringify(blogsData));
+    await writeFile("./blogsDB.json", JSON.stringify(blogsData, null, 2));
     return res.status(201).json({ message: "Blog created successfully" });
   } catch (err) {
-    return res.status(401).json({message : err})
+    return res.status(401).json({ message: err });
+  }
+});
+
+// Blog UPdate -> PATCH
+app.patch("/blogs/:id", async (req, res) => {
+  const { id } = req.params; // id = 5b505468-0b7a-4501-b8d0-49f8a2ca7d3b
+  const blog = blogsData.find((blog) => blog.id === id);
+
+  if (!blog) {
+    return res.status(404).json({ message: "Blog not found" });
+  }
+
+  const { title, content, author } = req.body;
+
+  if (title !== undefined) blog.title = title;
+  if (content !== undefined) blog.content = content;
+  if (author !== undefined) blog.author = author;
+
+  blog.updatedAt = new Date().toISOString();
+
+  try {
+    await writeFile("./blogsDB.json", JSON.stringify(blogsData, null, 2));
+    return res.status(201).json({ message: "Blog updated" });
+  } catch (err) {
+    return res.json({ message: "Blog failed to update" });
+  }
+});
+
+// Like
+app.post("/blogs/:id/likes", async (req, res) => {
+  const { id } = req.params;
+  const blog = blogsData.find((blog) => blog.id === id);
+
+  if (!blog) {
+    return res.status(404).json({ message: "Blog not found" });
+  }
+
+  blog.like += 1;
+
+  try {
+    await writeFile("./blogsDB.json", JSON.stringify(blogsData, null, 2));
+    return res.status(201).json({ message: "Blog Liked" });
+  } catch (err) {
+    return res.status(401).json({ message: "Failed to Like on a blog" });
+  }
+});
+
+// Comment
+app.post("/blogs/:id/comment", async (req, res) => {
+  const { id } = req.params;
+  const blog = blogsData.find((blog) => blog.id === id);
+
+  if (!blog) {
+    return res.status(404).json({ message: "Blog not found" });
+  }
+
+  const { user, text } = req.body;
+
+  const newComment = {
+    id: crypto.randomUUID(),
+    user,
+    text,
+    createdAt: new Date().toISOString(),
+  };
+
+  blog.comment.push(newComment);
+
+  try {
+    await writeFile("./blogsDB.json", JSON.stringify(blogsData, null, 2));
+    return res.status(201).json({ message: "Comment added" });
+  } catch (err) {
+    return res.json({ message: " failed to comment" });
+  }
+});
+
+// Blog Delete
+app.delete("/blogs/:id", async (req, res) => {
+  const { id } = req.params;
+  const blogIndex = blogsData.findIndex((blog) => blog.id === id); // 4
+
+  if (!blogIndex) {
+    return res.status(404).json({ message: "Blog not found" });
+  }
+
+  blogsData.splice(blogIndex, 1);
+
+  try {
+    await writeFile("./blogsDB.json", JSON.stringify(blogsData, null, 2));
+    return res.status(201).json({ message: "Blog deleted" });
+  } catch (err) {
+    return res.json({ message: " Failed to Delete a blog" });
   }
 });
 
 
-// Blog UPdate -> PATCH
-app.patch("/blogs/:id" , async (req , res) => {
-    const {id} = req.params // id = 5b505468-0b7a-4501-b8d0-49f8a2ca7d3b
-    const blog = blogsData.find((blog) => blog.id === id )
+// register
+app.post("/users/register" , async (req , res) => {
+  const {username , email , password} = req.body
 
-    if(!blog){
-      return res.status(404).json({message : "Blog not found"})
-    }
+  if(!username || !email || !password){
+    return res.json({message : "All field are required to register"})
+  }
 
-    const {title , content , author} = req.body
+  const user = {
+    id : crypto.randomUUID(),
+    username ,
+    email,
+    password,
+    createdAt : new Date().toISOString(),
+    updatedAt : new Date().toISOString()
+  }
+  usersData.push(user)
 
-    if(title !== undefined) blog.title = title
-    if(content !== undefined) blog.content = content
-    if(author !== undefined ) blog.author = author
+   try {
+    await writeFile("./usersDB.json", JSON.stringify(usersData, null, 2));
+    return res.status(201).json({ message: "User registered" });
+  } catch (err) {
+    return res.json({ message: " failed to register" });
+  }
 
-    blog.updatedAt = new Date().toISOString()
-
-    try {
-      await writeFile("./blogsDB.json" ,  JSON.stringify(blogsData))
-      return res.status(201).json({message : "Blog updated"})
-    } catch (err) {
-      return res.json({message : "Blog failed to update"})
-    }
 })
+
+
 
 app.listen(7000, () => {
   console.log("Server started at http://localhost:7000/");
